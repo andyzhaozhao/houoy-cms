60aa3f01d0ff3cfde341372bb717bed13d3a0407
andyzhaozhao <andy_zhaozhao@hotmail.com>
2017-10-12 09:06:44 +0800

    Add: init project

---
+++ src/main/java/com/houoy/cms/HouoyCmsApplication.java
@@ -0,0 +1,12 @@
+package com.houoy.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouoyCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouoyCmsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/houoy/cms/HouoyCmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.houoy.cms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HouoyCmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
